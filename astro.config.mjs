b498f977ce40e08173d0dc4fe9f4c9115d211999
fork-ikeda <ikeda@fork.co.jp>
2024-04-18 13:13:02 +0900

    fix install vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import expressiveCode from "astro-expressive-code";
+
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
@@ -9,5 +10,5 @@ export default defineConfig({
   integrations: [tailwind(), expressiveCode(), mdx()],
   prefetch: true,
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
