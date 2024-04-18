69771ae33632c328bd94a09c083da3a068b5980f
fork-ikeda <ikeda@fork.co.jp>
2024-04-18 13:20:17 +0900

    rm vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,8 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import expressiveCode from "astro-expressive-code";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), expressiveCode(), mdx()],
   prefetch: true,
-  output: "server",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
 });
