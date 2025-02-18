44992c33e0786efb5097150d71cfd87db4ab8889
fork-ikeda <ikeda@fork.co.jp>
2025-02-18 17:50:05 +0900

    set skelton

--- src/components/react/RandomPhotos.tsx
+++ src/components/react/RandomPhotos.tsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 
 const regex = /photo-\d+/;
 
 const RandomPhotos = () => {
   const [photos, setPhotos] = useState<ImageMetadata[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const imageFile = import.meta.glob<{ default: ImageMetadata }>(
@@ -18,27 +19,42 @@ const RandomPhotos = () => {
       const shuffled = data.sort(() => 0.5 - Math.random());
       setPhotos(shuffled.slice(0, 9));
     });
+
+    setLoading(false);
   }, []);
 
   return (
     <>
-      {photos.map((photo) => {
-        const href = photo.src.match(regex)?.[0] || "";
-        return (
-          <a
-            href={`/photos/${href}/`}
-            key={photo.src}
-            className="aspect-square block"
-          >
-            <img
-              src={photo.src}
-              className="h-full object-cover"
-              width={photo.width}
-              height={photo.height}
+      {loading ? (
+        <>
+          {[...Array(9)].map((_, index) => (
+            <div
+              key={index}
+              className="animate-pulse aspect-square bg-gray-300"
             />
-          </a>
-        );
-      })}
+          ))}
+        </>
+      ) : (
+        <>
+          {photos.map((photo) => {
+            const href = photo.src.match(regex)?.[0] || "";
+            return (
+              <a
+                href={`/photos/${href}/`}
+                key={photo.src}
+                className="aspect-square block"
+              >
+                <img
+                  src={photo.src}
+                  className="h-full object-cover"
+                  width={photo.width}
+                  height={photo.height}
+                />
+              </a>
+            );
+          })}
+        </>
+      )}
     </>
   );
 };
